43780f6ae8a21e20f5784eb2afe646f2a097b846
Chris Carlon <cjc25@cjc25.com>
2022-11-13 17:07:23 -0500

    Day 6 - exponential lanternfish
    An approach with updates in place using an array.

---
+++ lib/day6.ml
@@ -0,0 +1,31 @@
+open Core
+
+type result = int
+
+let run_array ls ct =
+  let inits =
+    List.hd_exn ls |> String.split ~on:',' |> List.map ~f:Int.of_string
+  in
+  let fish = Array.create ~len:9 0 in
+  List.iter inits ~f:(fun i -> Array.set fish i (fish.(i) + 1));
+  for _ = 1 to ct do
+    let zeroes = fish.(0) in
+    for i = 0 to 7 do
+      Array.set fish i fish.(i + 1)
+    done;
+    Array.set fish 6 (fish.(6) + zeroes);
+    Array.set fish 8 zeroes
+  done;
+  Array.fold fish ~init:0 ~f:(fun acc ct -> acc + ct)
+
+let parta ls = run_array ls 80
+let partb ls = run_array ls 256
+let sample = {|3,4,3,1,2|} |> String.split_lines
+
+let%expect_test "sample a" =
+  parta sample |> Int.to_string |> print_endline;
+  [%expect {| 5934 |}]
+
+let%expect_test "sample b" =
+  partb sample |> Int.to_string |> print_endline;
+  [%expect {| 26984457539 |}]
